21c03b87c49a098180fdb63f88b73f0ebf9a291c
Brian Adams <badams@gnarlie.com>
2012-07-26 19:07:05 -0500

    autopep

--- pyweb.py
+++ pyweb.py
@@ -7,23 +7,26 @@ import os
 import quick2wire.i2c as i2c
 import quick2wire.gpio as gpio
 
-address=0x20
-IODIR=0x00
-IPOL=0x01
-GPINTEN=0x02
-DEFVAL=0x03
-INTCON=0x04
-IOCON=0x05
-GPPU=0x06
-INTF=0x07
-GPIO=0x09
-OLAT=0x0A
+address = 0x20
+IODIR = 0x00
+IPOL = 0x01
+GPINTEN = 0x02
+DEFVAL = 0x03
+INTCON = 0x04
+IOCON = 0x05
+GPPU = 0x06
+INTF = 0x07
+GPIO = 0x09
+OLAT = 0x0A
 
-pin7 = gpio.Pin(11, gpio.Pin.In);
+pin7 = gpio.Pin(11, gpio.Pin.In)
+
 
 def _read(bus, register):
-    current = bus.transaction(i2c.write_bytes(address, register), i2c.read(address, 1))
+    current = bus.transaction(
+        i2c.write_bytes(address, register), i2c.read(address, 1))
     return current[0][0]
+
 
 def _toggle(pin, register):
     pinBitMask = 1 << pin
@@ -31,6 +34,7 @@ def _toggle(pin, register):
         current = _read(bus, register)
         next = (current & ~pinBitMask) | (pinBitMask ^ current) & 0xff
         bus.transaction(i2c.write_bytes(address, register, next))
+
 
 def _writeState(handler):
     interrupt = pin7.value
@@ -44,46 +48,53 @@ def _writeState(handler):
         intf = _read(bus, INTF)
         gpioStatus = _read(bus, GPIO)
         olat = _read(bus, OLAT)
-    handler.write("{\"gpio\": " + str(gpioStatus) + 
-            ", \"iodir\": " + str(dirStatus) +
-            ", \"gpinten\": " + str(gpinten) +
-            ", \"defval\": " + str(defval) +
-            ", \"intcon\": " + str(intcon) +
-            ", \"intf\": " + str(intf) +
-            ", \"olat\": " + str(olat) +
-            ", \"interrupt\": " + str(interrupt) +
-            "}")
+    handler.write("{\"gpio\": " + str(gpioStatus) +
+                  ", \"iodir\": " + str(dirStatus) +
+                  ", \"gpinten\": " + str(gpinten) +
+                  ", \"defval\": " + str(defval) +
+                  ", \"intcon\": " + str(intcon) +
+                  ", \"intf\": " + str(intf) +
+                  ", \"olat\": " + str(olat) +
+                  ", \"interrupt\": " + str(interrupt) +
+                  "}")
     handler.finish()
+
 
 class ToggleStateHandler(tornado.web.RequestHandler):
     def get(self, pin):
         _toggle(int(pin), GPIO)
         _writeState(self)
 
+
 class ToggleDirectionHandler(tornado.web.RequestHandler):
     def get(self, pin):
         _toggle(int(pin), IODIR)
         _writeState(self)
+
 
 class ToggleGpintenHandler(tornado.web.RequestHandler):
     def get(self, pin):
         _toggle(int(pin), GPINTEN)
         _writeState(self)
 
+
 class ToggleDefvalHandler(tornado.web.RequestHandler):
     def get(self, pin):
         _toggle(int(pin), DEFVAL)
         _writeState(self)
+
 
 class ToggleIntconHandler(tornado.web.RequestHandler):
     def get(self, pin):
         _toggle(int(pin), INTCON)
         _writeState(self)
 
+
 class ToggleOlatHandler(tornado.web.RequestHandler):
     def get(self, pin):
         _toggle(int(pin), OLAT)
         _writeState(self)
+
 
 class StatusHandler(tornado.web.RequestHandler):
     def get(self):
@@ -98,7 +109,7 @@ application = tornado.web.Application([
     (r"/olat/([0-7])", ToggleOlatHandler),
     (r"/status", StatusHandler),
     (r"/", tornado.web.RedirectHandler, dict(url="/index.html")),
-    (r"/(.*)", tornado.web.StaticFileHandler, 
+    (r"/(.*)", tornado.web.StaticFileHandler,
         dict(path=os.path.join(os.path.dirname(__file__), "www")))
 ])
 
@@ -107,4 +118,3 @@ if __name__ == "__main__":
     with i2c.I2CBus() as bus:
         bus.transaction(i2c.write_bytes(address, 0x4, 0xff))
     tornado.ioloop.IOLoop.instance().start()
-
